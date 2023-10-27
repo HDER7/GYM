47a4d30575203ba553fa065c932ecdbf378ac3f9
Heider Rivas <heiderarb@gmail.com>
2023-10-27 18:23:14 -0500

    Haciendo conecxion a BD(firebase)

--- gym_app/context/firebase/firebaseStage.js
+++ gym_app/context/firebase/firebaseStage.js
@@ -1,7 +1,7 @@
-import React, {useReducer} from 'react'
-import firebase from '../../firebase'
-import FirebaseContext from './firebaseContext'
-import FirebaseReducer from './firebaseReducer'
+import React, {useReducer} from 'react';
+import firebase from '../../firebase';
+import FirebaseContext from './firebaseContext';
+import FirebaseReducer from './firebaseReducer';
 
 const FirebaseStage = props => {
     const initialStage = {
