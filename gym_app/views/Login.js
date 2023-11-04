93e185a2804e914d733851f7c1e75b1f5c50a03b
Heider Rivas <heiderarb@gmail.com>
2023-11-04 08:27:16 -0500

    Update files

--- gym_app/views/Login.js
+++ gym_app/views/Login.js
@@ -1,10 +1,72 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  Box,
+  Heading,
+  VStack,
+  FormControl,
+  Input,
+  Link,
+  Button,
+  NativeBaseProvider,
+  Center,
+} from 'native-base';
+import {SSRProvider} from '@react-aria/ssr';
 
 function Login() {
-    return (
-        <View><Text>Login</Text></View>
-    )
+  return (
+    <SSRProvider>
+      <NativeBaseProvider>
+        <Center w="100%">
+          <Box safeArea p="2" py="40%" w="100%" maxW="370">
+            <Heading
+              size="lg"
+              fontWeight="900"
+              color="blue.700"
+              _dark={{
+                color: 'warmGray.50',
+              }}>
+              ¡Bienvenido a GYMFLASH!
+            </Heading>
+            <Heading
+              mt="1"
+              _dark={{
+                color: 'warmGray.500',
+              }}
+              color="coolGray.800"
+              fontWeight="medium"
+              size="lg">
+              Inicio de sesion
+            </Heading>
+
+            <VStack space={3} mt="8">
+              <FormControl>
+                <FormControl.Label>Email</FormControl.Label>
+                <Input size="lg" />
+              </FormControl>
+              <FormControl>
+                <FormControl.Label>Contraseña</FormControl.Label>
+                <Input size="lg" type="password" />
+                <Link
+                  _text={{
+                    fontSize: 'md',
+                    fontWeight: '500',
+                    color: 'blue.700',
+                  }}
+                  alignSelf="flex-end"
+                  mt="1">
+                  Olvidaste tu contraseña?
+                </Link>
+              </FormControl>
+              <Button mt="2" colorScheme="blue">
+                Iniciar Sesion
+              </Button>
+            </VStack>
+          </Box>
+        </Center>
+      </NativeBaseProvider>
+    </SSRProvider>
+  );
 }
 
-export default Login
+export default Login;
--- gym_app/views/Training.js
+++ gym_app/views/Training.js
@@ -3,5 +3,5 @@ import React from 'react'
 export default function Training() {
     return (
         <div>Training</div>
-    )
+    );
 }
